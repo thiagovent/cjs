f351afd27910979905acb00ffb35cb0ed6f87dd9
Thiago Venturelli <thiago.venturelli@fluig.com>
2019-04-01 10:37:00 -0300

    script for youtube, remove comments and videos from side bar

---
+++ youtube.js
@@ -0,0 +1,13 @@
+// requires jquery
+
+let cjs_youtube = setInterval(function() {
+  if (window.location.href.indexOf("watch") == 0) {
+      clearInterval(cjs_youtube);
+  } else ($("#masthead-container")[0]) {
+      $("#related").remove();
+      $("#sections").remove();
+      $("#masthead-container").remove();
+      $("#page-manager")[0].style.marginTop = "-20px";
+      clearInterval(cjs_youtube);
+  }
+}, 500);
